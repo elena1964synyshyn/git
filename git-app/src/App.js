13c0c490c989cc3155bf618b83b979c3a7f7c455
elena <synyshyn1@gmail.com>
2026-04-22 19:50:50 +0200

    summary folder list.js and todolist.js and countercomponents.js and update app.js

--- git-app/src/App.js
+++ git-app/src/App.js
@@ -1,30 +1,30 @@
 import './App.css';
-// import CounterComponent from './CounterComponent';
-// import List from './List';
-// function App() {
-//   return (
-//     <div className="app">
-//       <header className="App-header">
-//         <List />
-//         <CounterComponent />
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import BookList from './Components/BookList';
-import TodoList from './Components/TodoList';
-
+import CounterComponent from './CounterComponent';
+import List from './List';
 function App() {
   return (
-    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
-      <h1> Бібліотека Стівена Кінга</h1>
-      <BookList />
-      <TodoList />
+    <div className="app">
+      <header className="App-header">
+        <List />
+        <CounterComponent />
+      </header>
     </div>
   );
 }
+
+// export default App;
+
+// import BookList from './Components/BookList';
+// import TodoList from './Components/TodoList';
+
+// function App() {
+//   return (
+//     <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+//       <h1> Бібліотека Стівена Кінга</h1>
+//       <BookList />
+//       <TodoList />
+//     </div>
+//   );
+// }
 
 export default App;
