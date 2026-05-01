bc7629626a1e45a457232982f2ff3e6a699bca85
elena1964synyshyn <102073307+elena1964synyshyn@users.noreply.github.com>
2026-05-01 16:56:20 +0200

    uodate C:\Users\SERYAL PC\Desktop\салон краси\homework\git\git-app\src\MyClassComponent.js

--- git-app/src/MyClassComponent.js
+++ git-app/src/MyClassComponent.js
@@ -2,15 +2,34 @@ import { Component } from 'react';
 
 class MyClassComponent extends Component {
   constructor(props) {
-    super(props);
-    this.state = { name: 'Olena' };
+    //   super(props);
+    //   this.state = { name: 'Olena' };
   }
+  state = {
+    todos: [],
+    input: '',
+  };
+  addTask()=> {
+    const value = e.target.value;
+    this.setState({
+      todos: [...this.state.todos, this.state.input],
+    });
+  }
+
+  onChangeHandler = (e) => {
+    const value = e.target.value;
+    this.setState({ input: value });
+  };
 
   render() {
     return (
-      <div>
-        <h1>{this.state.name}</h1>
-      </div>
+      <>
+        <input value={this.state.input} onChange={this.onChangeHandler} />
+        <button onClick={this.addTask}>Add Todo</button>
+        {this.state.todos.map((todo) => (
+          <p>{todo}</p>
+        ))}
+      </>
     );
   }
 }
